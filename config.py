44ebdc5cc8d559ed6ba5d28132d97e1c7f89a96e
Oleksandr <oleksandr.romaniuk.work@gmail.com>
2020-03-20 12:51:54 +0200

    initial

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Configuration(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'myfirstflaskapp'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
     ### Flask-security
